d12cb133e45106fb69c2f51a87667e7b2a0337ce
Soma Sekine <a8121151@aoyama.jp>
2023-12-13 21:20:44 -0500

    ボタンのコンポーネント化

--- src/components/ToDoAdd.vue
+++ src/components/ToDoAdd.vue
@@ -1,5 +1,15 @@
 <script>
+import MyButton from './basics/MyButton.vue'
+
 export default {
+  components: {
+    MyButton,
+  },
+  data() {
+    return {
+      newTodoText: '',
+    }
+  },
   emits: ['delete-done', 'add-todo'],
   methods: {
     todoAdd() {
@@ -11,7 +21,7 @@ export default {
 </script>
 
 <template>
-  <input type="text" v-model="newTodoText" /><button @click="todoAdd">
-    追加</button
-  ><button @click="$emit('delete-done')">完了済みを削除する</button>
+  <input type="text" v-model="newTodoText" /><MyButton @click="todoAdd">
+    追加 <img src="../assets/icon-plus.svg" height="10" /></MyButton
+  ><MyButton @click="$emit('delete-done')">完了済みを削除する</MyButton>
 </template>
